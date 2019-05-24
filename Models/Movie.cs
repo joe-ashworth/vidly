4c37ca10bd1164a40b2a62c0d1fb2d63bfc6510f
Joe Ashworth <joeashworth@gmail.com>
2019-05-24 14:57:57 -0500

    Add project files.

---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    // POCO = Plain Old CLR Object
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    // /movies/random
+}
